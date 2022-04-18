bc305357c48d79b0590539524521a214a52d2eb5
Jakov Glavina <jakov.glavina@icloud.com>
2022-04-18 14:32:38 +0200

    perf(next): replace babel with swc

--- apps/next/next.config.js
+++ apps/next/next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+};
 
-const withPlugins = require('next-compose-plugins')
-const withTM = require('next-transpile-modules')(['app'])
+const withPlugins = require('next-compose-plugins');
+const withTM = require('next-transpile-modules')(['app']);
 
-module.exports = withPlugins([withTM], nextConfig)
+module.exports = withPlugins([withTM], nextConfig);
