434f980131d3abe0b678ee570cf1d525efed4b4b
Jakov Glavina <jakov.glavina@icloud.com>
2022-04-21 15:35:25 +0200

    feat: google auth test

--- apps/expo/App.tsx
+++ apps/expo/App.tsx
@@ -1,10 +1,22 @@
 import 'react-native-gesture-handler';
-import { View, Text } from 'react-native';
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import * as Google from 'expo-auth-session/providers/google';
 
 const App = () => {
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    iosClientId: process.env.GOOGLE_AUTH_IOS_CLIENT_ID,
+  });
+
+  React.useEffect(() => {
+    console.log('RESPONSE', response);
+  }, [response]);
+
   return (
     <View style={{ marginTop: 50 }}>
-      <Text>Bok</Text>
+      <TouchableOpacity onPress={() => promptAsync()}>
+        <Text>Bok</Text>
+      </TouchableOpacity>
     </View>
   );
 };
