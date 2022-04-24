6b838ac6c9d60c6193c36db2f6ef9104b0994a62
Jakov Glavina <jakov.glavina@icloud.com>
2022-04-24 22:46:00 +0200

    feat: try out webrtc in expo project

--- apps/expo/App.tsx
+++ apps/expo/App.tsx
@@ -1,20 +1,46 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import * as Google from 'expo-auth-session/providers/google';
+import { mediaDevices } from 'react-native-webrtc';
 
 const App = () => {
-  const [request, response, promptAsync] = Google.useAuthRequest({
-    iosClientId: process.env.GOOGLE_AUTH_IOS_CLIENT_ID,
-  });
+  useEffect(() => {
+    const configuration = { iceServers: [{ url: 'stun:stun.l.google.com:19302' }] };
 
-  React.useEffect(() => {
-    console.log('RESPONSE', response);
-  }, [response]);
+    let isFront = true;
+    mediaDevices.enumerateDevices().then((sourceInfos) => {
+      console.log(sourceInfos);
+      let videoSourceId;
+      for (let i = 0; i < sourceInfos.length; i++) {
+        const sourceInfo = sourceInfos[i];
+        if (sourceInfo.kind == 'videoinput' && sourceInfo.facing == (isFront ? 'front' : 'environment')) {
+          videoSourceId = sourceInfo.deviceId;
+        }
+      }
+      mediaDevices
+        .getUserMedia({
+          audio: true,
+          video: {
+            width: 640,
+            height: 480,
+            frameRate: 30,
+            facingMode: isFront ? 'user' : 'environment',
+            deviceId: videoSourceId,
+          },
+        })
+        .then((stream) => {
+          // Got stream!
+        })
+        .catch((error) => {
+          // Log error
+        });
+    });
+  }, []);
 
   return (
     <View style={{ marginTop: 50 }}>
-      <TouchableOpacity onPress={() => promptAsync()}>
+      <TouchableOpacity onPress={() => {}}>
         <Text>Bok</Text>
       </TouchableOpacity>
     </View>
