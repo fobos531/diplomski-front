9e190bf732a45cc8321bdb4b12e77b31f10bcf1a
Jakov Glavina <jakov.glavina@icloud.com>
2022-04-24 23:04:06 +0200

    feat: landing page tweaks, add expo-av

--- apps/next/src/features/home/VideoBackground.tsx
+++ apps/next/src/features/home/VideoBackground.tsx
@@ -1,9 +1,10 @@
-const VideoBackground: React.FC = () => {
+const VideoBackground: React.FC = ({ children }) => {
   return (
-    <div>
-      <video autoPlay muted loop className="fixed min-h-full min-w-full brightness-50">
+    <div className="flex h-screen">
+      <video autoPlay muted loop className="fixed min-h-full min-w-full brightness-50 z-10">
         <source src="https://assets.mixkit.co/videos/preview/mixkit-set-of-plateaus-seen-from-the-heights-in-a-sunset-26070-large.mp4" />
       </video>
+      <div className="z-30 m-auto">{children}</div>
     </div>
   );
 };
--- apps/next/src/pages/index.tsx
+++ apps/next/src/pages/index.tsx
@@ -3,7 +3,14 @@ import { NextPage } from 'next';
 import VideoBackground from '@features/home/VideoBackground';
 
 const Home: NextPage = () => {
-  return <VideoBackground />;
+  return (
+    <VideoBackground>
+      <div>
+        <h1 className="text-white text-center">CineSimul</h1>
+        <h3 className="text-white text-center">Lose yourself in movies.</h3>
+      </div>
+    </VideoBackground>
+  );
 };
 
 export default Home;
