1537db9a0fcbcc8c67f9b9cd3e2d569556ccee5a
Jakov Glavina <jakov.glavina@icloud.com>
2022-04-29 12:54:22 +0200

    feat: introduce imgHelpers, WIP MovieScreen expo

--- apps/expo/src/features/movies/MovieScreen.tsx
+++ apps/expo/src/features/movies/MovieScreen.tsx
@@ -1,15 +1,20 @@
-import { View, Text, ScrollView, TouchableOpacity } from 'react-native';
+import { useRef, useState } from 'react';
+import { Text, ScrollView, TouchableOpacity, ImageBackground, Image, StyleSheet, View } from 'react-native';
+import { BlurView } from 'expo-blur';
+import Carousel from 'react-native-reanimated-carousel';
 import { useQuery } from 'react-query';
 import { RouteProp, useRoute } from '@react-navigation/native';
 import dayjs from 'dayjs';
 import YoutubePlayer, { YoutubeIframeRef } from 'react-native-youtube-iframe';
 
 import { getMovie } from 'app/features/movies/api/movies';
+import { getBackdropUrl, getPosterUrl } from 'app/misc/imgHelpers';
 
 import { HomeNavigatorParamList } from '@features/home/navigation/HomeStack';
 import screen from '@navigation/screens';
 import { fontSize } from '@constants/typography';
-import { useRef, useState } from 'react';
+import React from 'react';
+import { interpolate } from 'react-native-reanimated';
 
 interface MovieScreenProps {}
 
@@ -23,18 +28,82 @@ const MovieScreen: React.FunctionComponent<MovieScreenProps> = () => {
     ytRef.current?.seekTo(10, true);
   };
 
+  const animationStyle = React.useCallback((value: number) => {
+    'worklet';
+
+    const zIndex = interpolate(value, [-1, 0, 1], [10, 20, 30]);
+    const scale = interpolate(value, [-1, 0, 1], [1.25, 1, 0.25]);
+    const opacity = interpolate(value, [-0.75, 0, 1], [0, 1, 0]);
+
+    return {
+      transform: [{ scale }],
+      zIndex,
+      opacity,
+    };
+  }, []);
+
   return (
     <ScrollView>
       {movie && (
         <>
-          <Text>{movie.title || movie.original_title || movie.original_name}</Text>
+          <Text style={{ fontSize: 25, fontWeight: 'bold' }}>{movie.title || movie.original_title || movie.original_name}</Text>
           <Text>Released: {dayjs(movie.release_date).format('MMM DD, YYYY')}</Text>
-          <Text>Overview: {movie.overview}</Text>
+          <View style={{ height: 200 }}>
+            <ImageBackground source={{ uri: getBackdropUrl(movie.backdrop_path, 'w780') }} style={StyleSheet.absoluteFill} />
+            <BlurView style={{ flex: 1 }} intensity={10} />
+          </View>
+
+          <View style={{ flexDirection: 'row' }}>
+            <Image
+              source={{ uri: getPosterUrl(movie.poster_path, 'w342') }}
+              style={{ width: 150, height: 240, borderRadius: 10, marginRight: 10 }}
+            />
+
+            <Text style={{ flex: 1 }}>Overview: {movie.overview}</Text>
+          </View>
           <Text style={{ fontSize: fontSize.large }}>Videos</Text>
-          {movie.videos.results.map((v) => (
-            <Text>{v.name}</Text>
-          ))}
-          <YoutubePlayer ref={ytRef} height={300} play={playing} videoId={'81-34IJPlLM'} onChangeState={() => {}} />
+
+          <Carousel
+            height={200}
+            width={300}
+            style={{ width: '100%', marginLeft: 20 }}
+            pagingEnabled
+            snapEnabled
+            customConfig={() => ({ type: 'positive', viewCount: 10 })}
+            mode="horizontal-stack"
+            modeConfig={{
+              snapDirection: 'right',
+              stackInterval: 18,
+            }}
+            data={movie.images.backdrops}
+            renderItem={({ item }) => (
+              <Image source={{ uri: getBackdropUrl(item.file_path, 'w780') }} style={{ width: 300, height: 200 }} />
+            )}
+          />
+
+          {/*   <Carousel
+            height={200}
+            width={300}
+            style={{ width: '100%', marginLeft: 20 }}
+            pagingEnabled
+            snapEnabled
+            customConfig={() => ({ type: 'positive', viewCount: 10 })}
+            mode="horizontal-stack"
+            modeConfig={{
+              snapDirection: 'right',
+              stackInterval: 18,
+            }}
+            data={movie.images.posters}
+            renderItem={({ item }) => <Image source={{ uri: getPosterUrl(item.file_path, 'w780') }} style={{ width: 300, height: 200 }} />}
+          /> */}
+
+          {/* <Carousel
+            height={200}
+            width={300}
+            data={movie.videos.results}
+            renderItem={({ item }) => <YoutubePlayer height={300} videoId={item.key} onChangeState={() => {}} />}
+          /> */}
+
           <TouchableOpacity onPress={() => setPlaying(!playing)}>
             <Text>Toggle play/pause</Text>
           </TouchableOpacity>
---
+++ packages/app/misc/imgHelpers.ts
@@ -0,0 +1,20 @@
+type BackdropSize = 'w300' | 'w780' | 'w1280' | 'original';
+type LogoSize = 'w45' | 'w92' | 'w154' | 'w185' | 'w300' | 'w500' | 'original';
+type PosterSize = 'w92' | 'w154' | 'w185' | 'w342' | 'w500' | 'w780' | 'original';
+type StillSize = 'w92' | 'w185' | 'w300' | 'original';
+
+export const getBackdropUrl = (img: string, size: BackdropSize) => {
+  return `https://image.tmdb.org/t/p/${size}${img}`;
+};
+
+export const getLogoUrl = (img: string, size: LogoSize) => {
+  return `https://image.tmdb.org/t/p/${size}${img}`;
+};
+
+export const getPosterUrl = (img: string, size: PosterSize) => {
+  return `https://image.tmdb.org/t/p/${size}${img}`;
+};
+
+export const getStillUrl = (img: string, size: StillSize) => {
+  return `https://image.tmdb.org/t/p/${size}${img}`;
+};
