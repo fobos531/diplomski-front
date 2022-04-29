12c007ad7b55fdcea4615b636f0b53fe95fac81f
Jakov Glavina <jakov.glavina@icloud.com>
2022-04-29 15:06:29 +0200

    feat: movie page next wip

--- apps/next/src/pages/movie/[id].tsx
+++ apps/next/src/pages/movie/[id].tsx
@@ -1,15 +1,17 @@
+import { useRef } from 'react';
+import YouTube from 'react-youtube';
 import { GetServerSideProps, NextPage } from 'next';
 import dayjs from 'dayjs';
 import Image from 'next/image';
 import Youtube from 'react-youtube';
 
-import { getMovie } from 'app/features/movies/api/movies';
-import { Movie } from 'app/features/movies/types';
-import { useRef } from 'react';
-import YouTube from 'react-youtube';
+import { getMovie, getMovieCredits } from 'app/features/movies/api/movies';
+import { Credits, Movie } from 'app/features/movies/types';
+import { getBackdropUrl, getPosterUrl } from 'app/misc/imgHelpers';
 
 interface MovieProps {
   movie: Movie;
+  credits: Credits;
 }
 
 const Movie: NextPage<MovieProps> = ({ movie }) => {
@@ -21,11 +23,17 @@ const Movie: NextPage<MovieProps> = ({ movie }) => {
 
   return (
     <div>
+      {/*       TODO: // Fix gradient fade */}
+      <div style={{ height: 400, position: 'relative' }} className="bg-gradient-to-r from-purple-500 to-pink-500">
+        <Image src={getBackdropUrl(movie.backdrop_path, 'w1280')} alt="Movie poster" layout="fill" className="absolute" />
+      </div>
+
       <div>{movie.title || movie.original_title || movie.original_name}</div>
       <div>Released: {dayjs(movie.release_date).format('MMM DD, YYYY')}</div>
       <div>Genres: {movie.genres.map((g) => g.name).join(' ')}</div>
       <div>Status: {movie.status}</div>
-      <Image src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt="Movie poster" width={300} height={450} className="rounded" />
+
+      <Image src={getPosterUrl(movie.poster_path, 'w780')} alt="Movie poster" width={300} height={450} className="rounded" />
       <div>Average rating: {movie.vote_average}</div>
       <Youtube videoId="81-34IJPlLM" ref={ytRef} />
       <button onClick={handleSeek}>E boss</button>
@@ -37,10 +45,12 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   const id = context.query.id;
 
   const movie = await getMovie(Number(id));
+  const credits = await getMovieCredits(Number(id));
 
   return {
     props: {
       movie,
+      credits,
     },
   };
 };
