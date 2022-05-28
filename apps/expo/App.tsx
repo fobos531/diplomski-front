c2abb6efd8051071d04d84becb39b1296a04ddbd
Jakov Glavina <jakov.glavina@icloud.com>
2022-05-28 10:28:53 +0200

    feat: eject from expo, add livekit-react-native

--- apps/expo/App.tsx
+++ apps/expo/App.tsx
@@ -1,3 +1,4 @@
+import 'expo-dev-client';
 import 'react-native-gesture-handler';
 import { QueryClientProvider } from 'react-query';
 
--- apps/expo/index.js
+++ apps/expo/index.js
@@ -1,6 +1,9 @@
 import { registerRootComponent } from 'expo';
+import { registerGlobals } from 'livekit-react-native';
 
 import App from './App';
+
+registerGlobals();
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
