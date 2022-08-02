b2a7ba0defc8b3264217bd1c3b76dc06d641d149
Jakov Glavina <jakov.glavina@icloud.com>
2022-08-02 15:40:14 +0200

    chore: add vector icons

--- apps/expo/src/navigation/TabNavigator.tsx
+++ apps/expo/src/navigation/TabNavigator.tsx
@@ -1,5 +1,6 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import screen from '@navigation/screens';
 import HomeStack from '@features/home/navigation/HomeStack';
@@ -17,9 +18,17 @@ const TabNavigator = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <Tab.Navigator screenOptions={{ headerShown: false }}>
-        <Tab.Screen name="WebRTC" component={WebRTCScreen} />
-        <Tab.Screen name={screen.HOME_STACK} component={HomeStack} />
-        <Tab.Screen name={screen.SEARCH_STACK} component={SearchStack} />
+        <Tab.Screen
+          name="WebRTC"
+          component={WebRTCScreen}
+          options={{ tabBarIcon: ({ color, size }) => <Ionicons name="md-globe" size={size} color={color} /> }}
+        />
+        <Tab.Screen
+          name={screen.HOME_STACK}
+          component={HomeStack}
+          options={{ tabBarIcon: ({ color, size }) => <Ionicons name="home" size={size} color={color} /> }}
+        />
+        {/*         <Tab.Screen name={screen.SEARCH_STACK} component={SearchStack} /> */}
       </Tab.Navigator>
     </SafeAreaView>
   );
